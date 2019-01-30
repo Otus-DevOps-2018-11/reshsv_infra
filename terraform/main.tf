8f2b0bb60641feaadec50ac919ca3e1cabbdca14
Sergey Reshetnikov <reshsv@gmail.com>
2019-01-30 16:03:25 +0000

    Lab 9, create prod and stage environment, using terraform modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,67 +1,14 @@
 provider "google" {
-  version = "1.4.0"
-  project = "${var.project}"
-  region  = "${var.region}"
+version = "1.4.0"
+project = "${var.project}"
+region = "${var.region}"
 }
-
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  tags         = ["reddit-app"]
-
-  metadata {
-    ssh-keys = "reshetnikov:${file(var.public_key_path)}"
-  }
-
-  # определение загрузочного диска 
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  # определение сетевого интерфейса
-  network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
-
-    # использовать  ephemeral IP для доступа из Интернет
-    access_config {}
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "reshetnikov"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "storage-bucket" {
+source = "SweetOps/storage-bucket/google"
+version = "0.1.1"
+# Имена поменяйте на другие
+name = ["storage-bucket-tst1", "storage-bucket-tst2"]
 }
-
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  # Название сети, в которой действует правило
-  network = "default"
-
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
+output storage-bucket_url {
+value = "${module.storage-bucket.url}"
 }
